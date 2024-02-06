8f96f1dc1da203d115ebe3eb2d0b35eb189d43ac
Pritam <pritamnagarp2@gmail.com>
2024-02-06 22:21:19 +0530

    pull

--- src/componenets/Project.jsx
+++ src/componenets/Project.jsx
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Pagination } from 'swiper/modules'
 import 'swiper/css/pagination';
 import 'swiper/css';
-<<<<<<< HEAD
+
 const projects=[
         {
         id:"1",
@@ -22,17 +22,8 @@ const projects=[
         gitLink:"https://github.com/Nagar-18/MyBlog",
         liveLink:"https://bloggers-eight.vercel.app/",
         content:"when we hear a blog we thought a simple image and some content in it. we can create it through various language and thier framework. react-js and tailwind css is used for overall design React-Hook-Form for form functionality , React-Redux is used for state management and Appwrite as backend service is used"
-=======
-const projects=[{
-        id:"3",
-        imgsrc:"",
-        title:"StreamX",
-        gitLink:"https://github.com/Nagar-18/StreamX",
-        liveLink:"https://streamx-beta.vercel.app/",
-        content:"A realtime stream web-application like Netflix where user can filter the shows accordingly.	Fetching of real-time API  from tmdb data base  and  use the fetched data effectively.	Redux-toolkit is used for state and data management.User can filterOut shows as trending and on the basis of genres."
->>>>>>> a0cb92ab1e7647d46717307c25b12cb771647e1d
+        },
 
-},
 {
         id:"2",
         imgsrc:"",
@@ -42,23 +33,23 @@ const projects=[{
         content:"BERN states a website that is formed with the intergation of centralized and decentralized backend it is a small prototype for futurestic website like voting  application. the website perform crud operation where create and update are done on blockchain and other simple operation performed on server"
 
 },
-<<<<<<< HEAD
+// <<<<<<< HEAD
+// // {
+// //         id:"3",
+// //         imgsrc:"",
+// //         title:"StreamX",
+// //         gitLink:"https://github.com/Nagar-18/StreamX",
+// //         liveLink:"https://streamx-beta.vercel.app/",
+// //         content:"A realtime stream web-application like Netflix where user can filter the shows accordingly.	Fetching of real-time API  from tmdb data base  and  use the fetched data effectively.	Redux-toolkit is used for state and data management.User can filterOut shows as trending and on the basis of genres."
+// =======
 // {
-//         id:"3",
-//         imgsrc:"",
-//         title:"StreamX",
-//         gitLink:"https://github.com/Nagar-18/StreamX",
-//         liveLink:"https://streamx-beta.vercel.app/",
-//         content:"A realtime stream web-application like Netflix where user can filter the shows accordingly.	Fetching of real-time API  from tmdb data base  and  use the fetched data effectively.	Redux-toolkit is used for state and data management.User can filterOut shows as trending and on the basis of genres."
-=======
-{
-        id:"1",
-        imgsrc:"blogimg",
-        title:"BlogWebsite",
-        gitLink:"https://github.com/Nagar-18/MyBlog",
-        liveLink:"https://bloggers-eight.vercel.app/",
-        content:"when we hear a blog we thought a simple image and some content in it. we can create it through various language and thier framework. react-js and tailwind css is used for overall design React-Hook-Form for form functionality , React-Redux is used for state management and Appwrite as backend service is used"
->>>>>>> a0cb92ab1e7647d46717307c25b12cb771647e1d
+//         id:"1",
+//         imgsrc:"blogimg",
+//         title:"BlogWebsite",
+//         gitLink:"https://github.com/Nagar-18/MyBlog",
+//         liveLink:"https://bloggers-eight.vercel.app/",
+//         content:"when we hear a blog we thought a simple image and some content in it. we can create it through various language and thier framework. react-js and tailwind css is used for overall design React-Hook-Form for form functionality , React-Redux is used for state management and Appwrite as backend service is used"
+// >>>>>>> a0cb92ab1e7647d46717307c25b12cb771647e1d
 
 // },
 
